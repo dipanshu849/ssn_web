375dd2f593ae389ece7bfa7c7564d890fb4f9107
dipanshu849d <dipanshu849d@gmail.com>
2025-05-25 17:41:13 +0530

    calender section added

---
+++ src/components/calender_type_navigator.js
@@ -0,0 +1,47 @@
+const calender__type__navigator = () => {
+  const event__btn = document.querySelector(".calender__type-event");
+  const athletics__btn = document.querySelector(".calender__type-athletics");
+  const circulars__btn = document.querySelector(".calender__type-circulars");
+
+  const table__contents = document.querySelectorAll(".calender__type-content");
+
+  const type__btn = document.querySelectorAll(".calender__type");
+
+  let current__index = 0;
+
+  const update__calender__table = () => {
+    table__contents.forEach((item, index) => {
+      item.style.display = index === current__index ? "block" : "none";
+    });
+
+    type__btn.forEach((item, index) => {
+      item.style.backgroundColor =
+        index === current__index ? "var(--color-slate-200)" : "white";
+    });
+  };
+
+  const update__current__index = (type) => {
+    if (type == "events") {
+      current__index = 0;
+    } else if (type == "athletics") {
+      current__index = 1;
+    } else {
+      current__index = 2;
+    }
+
+    update__calender__table();
+  };
+
+  event__btn.addEventListener("click", () => {
+    update__current__index("events");
+  });
+  athletics__btn.addEventListener("click", () => {
+    update__current__index("athletics");
+  });
+  circulars__btn.addEventListener("click", () => {
+    update__current__index("circulars");
+  });
+  update__calender__table();
+};
+
+export default calender__type__navigator;
--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
+import "../styles/components/calender.css";
 import "../styles/utils.css";
 
 import featured__carousel from "./components/featured_slider.js";
+import calender__type__navigator from "./components/calender_type_navigator.js";
 
 featured__carousel();
+calender__type__navigator();
