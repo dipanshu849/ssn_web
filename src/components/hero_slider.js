ff5ffe807c6c17d525285045f57feb6c2bca0000
dipanshu849d <dipanshu849d@gmail.com>
2025-05-27 14:45:51 +0530

    hero__slider, principals message and footer section added

---
+++ src/components/hero_slider.js
@@ -0,0 +1,56 @@
+import { update } from "three/examples/jsm/libs/tween.module.js";
+
+const hero__img__slider = () => {
+  const navigator__btns = document.querySelectorAll(".hero__img-navigator-btn");
+  const hero__imgs = document.querySelectorAll(".hero__img");
+
+  let current__index = 0;
+  let prev__index = 0;
+
+  const update__slider__prev = () => {
+    hero__imgs.forEach((img, index) => {
+      if (index == prev__index && current__index !== prev__index) {
+        img.classList.add("inactive");
+        prev__index = current__index;
+        setTimeout(update__slider, 1500);
+      } else {
+        img.classList.remove("inactive");
+      }
+    });
+  };
+
+  const update__slider = () => {
+    hero__imgs.forEach((img, index) => {
+      img.classList.toggle("active", index === current__index);
+    });
+
+    navigator__btns.forEach((btn, index) => {
+      btn.classList.toggle("active", index === current__index);
+    });
+  };
+
+  const next__slide = () => {
+    current__index = (current__index + 1) % hero__imgs.length;
+    update__slider__prev();
+  };
+
+  const auto__slide = () => {
+    intervalID = setInterval(() => {
+      next__slide();
+    }, 7000);
+  };
+
+  navigator__btns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      // clearInterval(intervalID);
+
+      current__index = index;
+      update__slider__prev();
+      // auto__slide();
+    });
+  });
+  update__slider();
+  // auto__slide();
+};
+
+export default hero__img__slider;
--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
 import "../styles/components/calender.css";
+import "../styles/components/message.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
 
 import featured__carousel from "./components/featured_slider.js";
 import calender__type__navigator from "./components/calender_type_navigator.js";
+import hero__img__slider from "./components/hero_slider.js";
 
 featured__carousel();
 calender__type__navigator();
+hero__img__slider();
